bd2c7944fa3a7fae475fbf97861fed6488919534
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2024-01-27 21:42:20 +0530

    Backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const CookieParser = require('cookie-parser');
+const connection = require('./config/connect');
+
+const userRouter = require('./routes/userRouter');
+const feedbackRouter = require('./routes/feedbackRotuer');
+const catagoryRouter = require('./routes/catagoryRouter');
+const titleRouter =  require('./routes/titleRouter');
+const questionRouter = require('./routes/questionRouter');
+const feedbackRotuer = require('./routes/feedbackRotuer');
+
 
 const cors = require('cors');
 require("dotenv").config();
